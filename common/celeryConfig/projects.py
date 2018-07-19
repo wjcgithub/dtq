81c56c5c8e81ffe656bd014ca3eaf5acdb8a990f
wjcgithub <wjc163@sina.cn>
2018-07-19 10:19:47 +0800

    整理文件，update守护进程由root切换为celery

--- common/celeryConfig/projects.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-rootPath = '/home/evolution/PycharmProjects/celery_project/'
-exchange = 'first'
-broker_url='amqp://test:test@localhost:5672/'
-numprocs=2
-result_backend='elasticsearch://es1:9200/celery-task/taskinfo'
-# result_backend='redis://localhost/2'
-# result_backend='db+mysql://root:brave@localhost/celery_failure_task'
