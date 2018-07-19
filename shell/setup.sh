b048e297eda527c7669593cbbd26701b99709155
wjcgithub <wjc163@sina.cn>
2018-07-19 14:58:16 +0800

    修改setup脚本

--- shell/setup.sh
+++ shell/setup.sh
@@ -2,6 +2,40 @@
 
 # dtq setup script
 
+checkPython()
+{
+    #推荐版本V2.6.5
+    V1=3
+    V2=5
+    V3=all
+
+    echo need python version is : $V1.$V2.$V3
+
+    #获取本机python版本号。这里2>&1是必须的，python -V这个是标准错误输出的，需要转换
+    U_V1=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
+    U_V2=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $2}'`
+    U_V3=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $3}'`
+
+    echo your python version is : $U_V1.$U_V2.$U_V3
+
+    if [ $U_V1 -lt $V1 ];then
+        echo 'Your python version is not OK!(1)'
+        exit 1
+    elif [ $U_V1 -eq $V1 ];then
+        if [ $U_V2 -lt $V2 ];then
+            echo 'Your python version is not OK!(2)'
+            exit 1
+        elif [ $U_V2 -eq $V2 ];then
+	    if [[ $U_V3 -lt $V3 || $V3 != all ]];then
+                echo 'Your python version is not OK!(3)'
+                exit 1
+            fi
+        fi
+    fi
+
+    echo Your python version is OK!
+}
+checkPython
 
 # create user
 user=celery
@@ -40,7 +74,7 @@ read pip3path
 if [ -e $pip3path ]
 then
 	echo "You pip3 bin path is $pip3path"
-	$pip3path install -U pymysql logging warnings redis celery html
+	$pip3path install -U pymysql logging warnings redis celery html flower
 else
 	echo "You inputs bin path of pip not exists"
 fi
