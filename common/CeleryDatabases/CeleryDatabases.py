ab579d810ad5cb936c6b43e1004e2a45cd019981
王继超 <wangjichao@xin.com>
2018-08-13 10:30:22 +0800

    数据库链接换成orm

--- common/CeleryDatabases/CeleryDatabases.py
+++ common/CeleryDatabases/CeleryDatabases.py
@@ -1,31 +1,68 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import pymysql
+
+from contextlib import contextmanager
+
+from .session import SessionManager
 from celeryConfig import mysqlconfig as config
 from clog.clog import logger
+from .Models.Groups import Groups
+from .Models.Queues import Queues
 
 class CeleryDatabases(object):
 
-    def getConnect(self):
-        conn = pymysql.connect(config.db_host, config.db_user, config.db_pass,config.db_name,
-                                      use_unicode=True, charset="utf8", cursorclass=pymysql.cursors.DictCursor)
-        return conn
+    def __init__(self):
+        self.engine_options = {}
+        self.url = 'mysql://%s:%s@%s:3306/%s' % (config.db_user, config.db_pass, config.db_host, config.db_name)
 
-
-    def execute_query(self, query, args=(), return_one=False, exec_many=False):
-        conn = self.getConnect()
+    @contextmanager
+    def session_cleanup(self,session):
         try:
-            with conn.cursor() as cursor:
-                cursor.execute('SET NAMES utf8;')
-                cursor.execute('SET CHARACTER SET utf8;')
-                cursor.execute('SET character_set_connection=utf8;')
-                if exec_many:
-                    cursor.executemany(query, args)
-                else:
-                    cursor.execute(query, args)
-        except Exception as e:
-            logger.error('查询数据错误:%s' % (str(query)))
+            yield
+        except Exception as exc:
+            session.rollback()
+            logger.error('Thread Timer crashed: %r', exc, exc_info=True)
+            raise
         finally:
-            conn.close()
-            del conn
-        return cursor.fetchone() if return_one else cursor.fetchall()
+            session.close()
+
+
+    def getSession(self, session_manager=SessionManager()):
+        """Get mysql session"""
+        return session_manager.session_factory(
+            dburi=self.url,
+            **self.engine_options)
+
+    def get_group_by_status(self,gid=None,return_one=False,status=None):
+        """Get group meta-data for a group by id."""
+        session = self.getSession()
+        q = session.query(Groups)
+        with self.session_cleanup(session):
+            if gid is not None:
+                q = q.filter(Groups.id == gid)
+
+            if status is not None:
+                q = q.filter(Groups.status == status)
+
+            rest = q.first() if return_one else q.all()
+            if rest:
+                return rest
+
+        return None
+
+    def get_queue_by_status(self,qid=None,return_one=False,status=None):
+        """Get queue meta-data for a queue by id."""
+        session = self.getSession()
+        q = session.query(Queues).filter(Queues.id == qid)
+        with self.session_cleanup(session):
+            if qid is not None:
+                q = q.filter(Queues.id == qid)
+
+            if status is not None:
+                q = q.filter(Queues.status == status)
+
+            rest = q.first() if return_one else q.all()
+            if rest:
+                return rest
+
+        return None
