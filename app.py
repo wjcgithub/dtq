fb96406813111b85f7dbac1e5c38d3136cf109ab
王继超 <wangjichao@xin.com>
2018-08-15 16:19:29 +0800

    delete comments

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
 # _*_ coding:utf-8 _*_
-import os
 from update_config.updateConfig import UpdateConfig
 
 if __name__ == '__main__':
-    # os.environ.setdefault('ENV', 'local')
-    # print(os.environ.get('ENV'))
     u = UpdateConfig()
     u.start()
